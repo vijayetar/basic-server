22b149a62bb5c735c53d8449284dd5c1719bdf06
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-10 17:31:19 -0800

    updated public profile

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT|| 3001;
 
-app.get('/', (request,response) => response.send(('Hello hey this is my server')));
-app.listen(PORT, ()=> console.log('app is up and running'));
+app.use(express.static('./public'));
+
+// app.get('/', (request,response) => response.send(('Hello hey this is my server')));
+app.get('/hello', (request,response) => response.send(('Hello hey this is my server')));
+app.listen(PORT, ()=> console.log(`app is up and running: ${PORT}`));

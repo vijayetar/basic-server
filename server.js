c7e357dff6c8044cce6248d3e08f623f10dc3f96
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-13 17:01:24 -0800

    made some changes to the server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 'use strict';
-const express = require('express');
+// require the libraries
+const express = require('express'); //you have to do npm 
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT|| 3001;
+const cors = require('cors'); //cors is the policeman
 
 app.use(express.static('./public'));
+app.use(cors()); 
+//cors only needed because our server doesnt have a front end, but the front end is out there
 
+//routes
 // app.get('/', (request,response) => response.send(('Hello hey this is my server')));
 app.get('/hello', (request,response) => response.send(('Hello hey this is my server')));
+
+app.get('/allbase', (request, response) => {
+  response.send('Are belong to us!')
+});
+
+app.get('*',(request, response) => {response.status(404).send('this round does not exist')});
+// turn the PORT on
 app.listen(PORT, ()=> console.log(`app is up and running: ${PORT}`));
